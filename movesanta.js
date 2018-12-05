b5f3081353c41d33181e03ce06043b70485cee13
Kristoffer Rene Eckhoff <kristoffereckhoff@gmail.com>
2018-12-05 14:16:31 +0100

    Update movesanta.js
    Kommentert kode = dårlig julesteming 🎅

--- movesanta.js
+++ movesanta.js
@@ -2,8 +2,6 @@
     var b;
     var x = 0,
       y = 0;
-    /*var dx = Math.random()*300;
-    var dy = Math.random()*300;*/
     var dx = 600;
     var dy = 80;
     b = document.getElementById('santa');
@@ -12,12 +10,8 @@
       x += (dx - x) * 0.075;
       y += (dy - x) * 0.075;
       b.style.left = x + 'px';
-      /*b.style.top = y + "px";*/
       b.style.top = 280 + 'px';
       if (Math.abs(x - dx) < 1) {
-        /*    dx = Math.random()*300;
-        dy = Math.random()*300;
-        x = 10;*/
         if (dx > 10) {
           b.style.transform = 'scaleX(-1)';
           dx = 10;
